73b0591bd7a11f2deee79ee6cad3364949940448
abantika27 <96277040+abantika27@users.noreply.github.com>
2023-01-19 11:19:05 +0530

    Update addtwoLL.cpp

--- LinkedList/addtwoLL.cpp
+++ LinkedList/addtwoLL.cpp
@@ -9,26 +9,28 @@
         }
         return prev;
     }
-    ListNode* add(ListNode* l1, ListNode* l2){
+     ListNode* add(ListNode* l1, ListNode* l2){
         ListNode* head= new ListNode(-1);
         ListNode* curr= head;
         int carry=0;
-        while (l1 || l2 || carry>0){
-            int v1=0, v2=0;
-            if (l1)v1= l1->val;
-            if (l2)v2= l2->val;
-            int sum= v1+v2+carry;
+        while (l1!=NULL || l2!=NULL || carry!=0){
+           int sum= carry+ (l1!=NULL?l1->val:0)+(l2!=NULL?l2->val:0);
             carry= sum/10;
             sum= sum%10;
             curr->next= new ListNode(sum);
             curr= curr->next;
-            if (l1)l1= l1->next;
-            if (l2)l2= l2->next;
+            if (l1!=NULL){
+                l1= l1->next;}
+            if (l2!=NULL){
+            l2= l2->next;}
         }
         head= head->next;
         return head;
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(l1==NULL || l2==NULL){
+            return l1!=NULL ? l1 : l2;
+        }
         l1= reverse(l1);
         l2= reverse(l2);
         ListNode* ans= add(l1,l2);
