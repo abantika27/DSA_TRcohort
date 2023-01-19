ea7bd4819642f20fcdc965146d50e5a4c73aa8e9
abantika27 <96277040+abantika27@users.noreply.github.com>
2023-01-19 10:32:42 +0530

    Update Intersection.cpp

--- LinkedList/Intersection.cpp
+++ LinkedList/Intersection.cpp
@@ -12,3 +12,40 @@ ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 		}
 		return NULL;
     }
+
+//floyd cycle detection method
+ListNode *startofCycle(ListNode *head){
+        if(head==NULL||head->next==NULL){
+            return head;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(slow!=fast){
+                return NULL;//cycle not present
+            }
+        slow=head;
+        fast=fast;//don't move fast
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return slow;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        //detection of floyd cycle
+        ListNode* tail=headA;
+        while(tail->next!=NULL){
+            tail=tail->next;
+        }
+        tail->next=headB;
+        ListNode* ans=startofCycle(headA);
+        tail->next=NULL;
+        return ans;
+    }
